f9356f51f57c95ee3b33ea88e56b61c714ca4b4a
Araryarch <Araryaarch@gmail.com>
2024-05-08 23:54:37 +0700

    feat: Install Recoil and develop task summary page

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,43 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 import SideMenu from './Components/Sidemenu'
+import TaskSummary from './features/tasks/Components/TaskSummary'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="flex">
-              <SideMenu />
-              <h1>Home</h1>
-            </div>
-          }
-        />
-        <Route
-          path="/task-list"
-          element={
-            <div className="flex">
-              <SideMenu />
-              <h1>Task List</h1>
-            </div>
-          }
-        />
-        <Route
-          path="/task-progress"
-          element={
-            <div className="flex">
-              <SideMenu />
-              <h1>Task Progress</h1>
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="flex">
+                <SideMenu />
+                <TaskSummary />
+              </div>
+            }
+          />
+          <Route
+            path="/task-list"
+            element={
+              <div className="flex">
+                <SideMenu />
+                <h1>Task List</h1>
+              </div>
+            }
+          />
+          <Route
+            path="/task-progress"
+            element={
+              <div className="flex">
+                <SideMenu />
+                <h1>Task Progress</h1>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   )
 }
 
