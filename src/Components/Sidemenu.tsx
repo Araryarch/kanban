6b39ab2c59943b7b80c1a4b73510fc81fd81d6fe
Araryarch <Araryaarch@gmail.com>
2024-05-09 12:20:54 +0700

    lupa dah

--- src/Components/Sidemenu.tsx
+++ src/Components/Sidemenu.tsx
@@ -9,20 +9,24 @@ const SideMenu = (): JSX.Element => {
 
   return (
     <div className="containers">
-      <NavLink to="/" className={`side-text ${isActive('/') ? 'text-blue-950' : 'text-white'}`}>
+      <h1 className="text-3xl text-white">Kanban App</h1>
+      <NavLink
+        to="/"
+        className={`side-text ${isActive('/') ? 'bg-blue-400 text-blue-950' : 'text-white'}`}
+      >
         <span className="material-icons icons">home</span>
         <p>Home</p>
       </NavLink>
       <NavLink
         to="/task-list"
-        className={`side-text ${isActive('/task-list') ? 'text-blue-950' : 'text-white'}`}
+        className={`side-text ${isActive('/task-list') ? 'bg-blue-400 text-blue-950' : 'text-white'}`}
       >
         <span className="material-icons icons">menu</span>
         <p>Task List</p>
       </NavLink>
       <NavLink
         to="/task-progress"
-        className={`side-text ${isActive('/task-progress') ? 'text-blue-950' : 'text-white'}`}
+        className={`side-text ${isActive('/task-progress') ? 'bg-blue-400 text-blue-950' : 'text-white'}`}
       >
         <span className="material-icons icons">check_box</span>
         <p>Task Progress</p>
