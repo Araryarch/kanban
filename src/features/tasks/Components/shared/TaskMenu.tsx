0950e47f4afffa02a3d27532e2b6d414ea6a3f43
Araryarch <Araryaarch@gmail.com>
2024-05-12 19:37:02 +0700

    A

--- src/features/tasks/Components/shared/TaskMenu.tsx
+++ src/features/tasks/Components/shared/TaskMenu.tsx
@@ -1,15 +1,20 @@
-import { Dispatch, SetStateAction } from 'react'
-
+import { Dispatch, SetStateAction, useState } from 'react'
+import TaskModal from '../shared/TaskModal'
+import { TASK_MODAL_TYPE, TASK_PROGRESS_ID } from '../../../../constants/app'
 interface TaskMenuProps {
   setIsMenuOpen: Dispatch<SetStateAction<boolean>>
   responsive: string
 }
 
 const TaskMenu = ({ setIsMenuOpen, responsive }: TaskMenuProps): JSX.Element => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
   return (
     <div className={`${responsive} menu`}>
       <div className="close-wrapper flex justify-end gap-2 p-1">
-        <div className="menuItem flex-1 hover:bg-white hover:text-[#3da9fc]">
+        <div
+          className="menuItem flex-1 hover:bg-white hover:text-[#3da9fc]"
+          onClick={(): void => setIsModalOpen(true)}
+        >
           <span className="material-icons px-1">edit</span>
           Edit
         </div>
@@ -20,6 +25,14 @@ const TaskMenu = ({ setIsMenuOpen, responsive }: TaskMenuProps): JSX.Element => 
       <div className="menuItem hover:bg-white hover:text-red-600">
         <span className="material-icons px-1">delete</span>Delete
       </div>
+      {isModalOpen && (
+        <TaskModal
+          setIsModalOpen={setIsModalOpen}
+          type={TASK_MODAL_TYPE.EDIT}
+          headingTitle="Edit your task"
+          defaultProgressOrder={TASK_PROGRESS_ID.NOT_STARTED}
+        />
+      )}
     </div>
   )
 }
